197f2d8b756137d8d648d058699008aef5d23198
bp26 <aleshak260006@gmail.com>
2024-01-02 22:44:42 +0300

    feat: add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module', 'nuxt-primevue'],
   devtools: { enabled: true }
 })
