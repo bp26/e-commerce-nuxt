8e063b0f3c01edd120f9c9066e73ef179569c8be
bp26 <aleshak260006@gmail.com>
2024-01-08 00:44:15 +0300

    feat: add TheHeader component

---
+++ components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="w-full shadow-md">
+    <div
+      class="m-auto flex h-16 max-w-screen-2xl items-center justify-between gap-2 px-2 py-5 sm:px-10 lg:h-20 2xl:px-0"
+    >
+      <TheHeaderLogo />
+
+      <div class="w-2/3 lg:w-2/5">
+        <TheHeaderSearch />
+      </div>
+
+      <TheHeaderNavigation />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
