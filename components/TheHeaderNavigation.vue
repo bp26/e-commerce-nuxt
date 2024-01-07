bb4f5aa9e159ac395983e090dcada308281c9e28
bp26 <aleshak260006@gmail.com>
2024-01-08 00:43:27 +0300

    feat: add TheHeaderNavigation component

---
+++ components/TheHeaderNavigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <TheHeaderNavigationUpper :links="links" />
+  <TheHeaderNavigationBottom :links="links" />
+</template>
+
+<script setup lang="ts">
+const links = [
+  {
+    to: '/',
+    text: 'Home',
+    icon: 'pi-home'
+  },
+  {
+    to: '/about',
+    text: 'About',
+    icon: 'pi-info-circle'
+  },
+  {
+    to: '/contact',
+    text: 'Contact',
+    icon: 'pi-id-card'
+  },
+  {
+    to: '/profile',
+    text: 'Profile',
+    icon: 'pi-user'
+  },
+  {
+    to: '/cart',
+    text: 'Cart',
+    icon: 'pi-shopping-cart'
+  }
+]
+</script>
