4baa0d86726e7747a06eec567e2f0b6ec5ef65dc
bp26 <aleshak260006@gmail.com>
2024-01-08 00:39:52 +0300

    feat: add BaseLink component

---
+++ components/Base/BaseLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <NuxtLink :to="to" class="group flex cursor-pointer flex-col items-center gap-1 p-3">
+    <i
+      v-if="icon"
+      :class="[
+        icon ? icon : '',
+        active ? 'text-link-active' : 'group-hover:text-link',
+        'pi lg:text-xl'
+      ]"
+    ></i>
+    <span :class="[active ? 'text-link-active' : 'group-hover:text-link', 'lg:text-lg']">
+      <slot></slot>
+    </span>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+    default: ''
+  },
+  active: {
+    type: Boolean,
+    default: false
+  },
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
