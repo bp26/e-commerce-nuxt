f74c87194fe9ca7be85b33a17b49ae085970cb5d
bp26 <aleshak260006@gmail.com>
2024-01-08 00:39:19 +0300

    feat: add TheHeaderSearch component

---
+++ components/TheHeaderSearch.vue
@@ -0,0 +1,10 @@
+<template>
+  <span class="p-input-icon-left w-full">
+    <i class="pi pi-search"></i>
+    <InputText placeholder="Search" class="w-full" />
+  </span>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
