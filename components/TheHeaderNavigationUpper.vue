264f78d51f22363dae4aff28f887d1cb2a12bb9f
bp26 <aleshak260006@gmail.com>
2024-01-08 00:41:47 +0300

    feat: add TheHeaderNavigationUpper component

---
+++ components/TheHeaderNavigationUpper.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="hidden lg:block">
+    <ul class="flex gap-2 xl:gap-6">
+      <li v-for="link in links" :key="link.to" class="flex-1">
+        <BaseLink :to="link.to" :active="isActiveLink(link.to)" :icon="link.icon">
+          {{ link.text }}
+        </BaseLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+const isActiveLink = useActiveLink()
+defineProps<{
+  links: {
+    to: string
+    text: string
+    icon: string
+  }[]
+}>()
+</script>
