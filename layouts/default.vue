deb77605ba5db0210fea3ed131e8cef0b72ec90e
bp26 <aleshak260006@gmail.com>
2024-01-08 00:44:40 +0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-full w-full">
+    <TheHeader />
+    <main class="m-auto max-w-screen-2xl">
+      <slot></slot>
+    </main>
+
+    <footer></footer>
+  </div>
+</template>
