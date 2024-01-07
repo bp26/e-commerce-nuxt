dc1caa580cbcf2b15303858e322811f4d85bb5d6
bp26 <aleshak260006@gmail.com>
2024-01-08 00:40:32 +0300

    feat: add BaseLinkIcon component

---
+++ components/Base/BaseLinkIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLink :to="to" class="group block w-full cursor-pointer py-3 text-center sm:py-4">
+    <i
+      v-if="icon"
+      :class="[
+        icon ? icon : '',
+        active ? 'text-link-active' : ' group-hover:text-link group-active:animate-bounce',
+        'pi m-auto text-2xl sm:text-3xl'
+      ]"
+      s
+    ></i>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+    default: ''
+  },
+  active: {
+    type: Boolean,
+    default: false
+  },
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
