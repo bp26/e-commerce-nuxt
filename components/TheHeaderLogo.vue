12e88a630659d7e682e6028b3bf40e925b9bba5d
bp26 <aleshak260006@gmail.com>
2024-01-08 00:38:40 +0300

    feat: add TheHeaderLogo component

---
+++ components/TheHeaderLogo.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLink to="/" class="flex cursor-pointer items-center gap-2">
+    <i class="pi pi-shopping-bag text-primary md:text-4xl"></i>
+    <h1 class="font-mono text-xl font-bold md:text-2xl">
+      <span class="whitespace-nowrap">E-com</span>
+      <span class="whitespace-nowrap text-primary">merce</span>
+    </h1>
+  </NuxtLink>
+</template>
