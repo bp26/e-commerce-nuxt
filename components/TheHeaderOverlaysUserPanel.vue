a261d811d3af8d210c177a93e29fc68e65a10b9d
bp26 <aleshak260006@gmail.com>
2024-01-10 14:25:19 +0300

    feat: add TheHeaderOverlaysUserPanel component

---
+++ components/TheHeaderOverlaysUserPanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <OverlayPanel ref="panel">
+    <ul class="flex w-40 flex-col gap-3">
+      <li>
+        <BaseLinkIcon icon="pi-user">Account</BaseLinkIcon>
+      </li>
+      <li>
+        <BaseLinkIcon icon="pi-shopping-bag">Orders</BaseLinkIcon>
+      </li>
+      <li>
+        <BaseLinkIcon icon="pi-heart">Wishlist</BaseLinkIcon>
+      </li>
+      <li>
+        <BaseLinkIcon icon="pi-power-off">Log out</BaseLinkIcon>
+      </li>
+    </ul>
+  </OverlayPanel>
+</template>
+
+<script setup lang="ts">
+import type OverlayPanel from 'primevue/overlaypanel'
+
+const panel = ref<OverlayPanel>()
+defineExpose({ panel })
+</script>
+
+<style scoped></style>
