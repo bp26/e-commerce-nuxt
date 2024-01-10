99a0942b25f1b79e2c6c8de03f6be737c33ddd4d
bp26 <aleshak260006@gmail.com>
2024-01-10 14:15:54 +0300

    feat: add BaseIconInteractive component

---
+++ components/Base/BaseIconInteractive.vue
@@ -0,0 +1,12 @@
+<template>
+  <i :class="[icon, 'pi cursor-pointer group-hover:text-link group-active:text-link']"></i>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
