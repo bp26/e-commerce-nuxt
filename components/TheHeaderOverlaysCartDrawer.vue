ab5401259eb40380ba5e5b9e1bec97648382155f
bp26 <aleshak260006@gmail.com>
2024-01-10 14:26:50 +0300

    feat: add TheHeaderOverlaysCartDrawer component

---
+++ components/TheHeaderOverlaysCartDrawer.vue
@@ -0,0 +1,10 @@
+<template>
+  <BaseDrawerMobile v-model:visible="isCartOpen"></BaseDrawerMobile>
+</template>
+
+<script setup lang="ts">
+const store = useInteractivityStore()
+const { isCartOpen } = storeToRefs(store)
+</script>
+
+<style scoped></style>
