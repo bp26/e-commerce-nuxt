1a23475ea9890c42e198c4f35af67c12abf07c14
bp26 <aleshak260006@gmail.com>
2024-01-10 14:30:00 +0300

    feat: add TheHeaderOverlaysSearchModal component

---
+++ components/TheHeaderOverlaysSearchModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <Dialog v-model:visible="isSearchOpen" modal class="h-[500px] w-[700px] bg-white">
+    <template #header><span></span></template>
+    <template #closeicon>
+      <IconClose />
+    </template>
+
+    <TheHeaderOverlaysSearchBar />
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+const store = useInteractivityStore()
+const { isSearchOpen } = storeToRefs(store)
+</script>
+
+<style scoped></style>
