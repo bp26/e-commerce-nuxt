ef7347ed5f3640de93952ed84af94e3ee24640d0
bp26 <aleshak260006@gmail.com>
2024-01-10 14:15:27 +0300

    feat: add NAVIGATION_LINKS constant

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const NAVIGATION_LINKS = [
+  {
+    to: '/',
+    text: 'Home'
+  },
+  {
+    to: '/about',
+    text: 'About'
+  },
+  {
+    to: '/contact',
+    text: 'Contact Us'
+  }
+] as const
