daf899cd416d04d59b8a44a51ae7cf13659cad28
bp26 <aleshak260006@gmail.com>
2024-01-10 14:24:01 +0300

    feat: add BaseDrawerMobile component

---
+++ components/Base/BaseDrawerMobile.vue
@@ -0,0 +1,26 @@
+<template>
+  <Sidebar
+    position="bottom"
+    class="h-full w-full bg-white"
+    :visible="visible"
+    @update:visible="$emit('update:visible')"
+  >
+    <template #header><span></span></template>
+    <template #closeicon>
+      <IconClose />
+    </template>
+    <slot></slot>
+  </Sidebar>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  visible: {
+    type: Boolean,
+    default: false
+  }
+})
+defineEmits(['update:visible'])
+</script>
+
+<style scoped></style>
