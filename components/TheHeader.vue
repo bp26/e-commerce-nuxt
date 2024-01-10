9c6d89e79d9567d325c0f20e010d6db3ec0bfd87
bp26 <aleshak260006@gmail.com>
2024-01-10 14:31:36 +0300

    feat: update TheHeader component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,19 +1,21 @@
 <template>
-  <header class="w-full shadow-md">
+  <header class="w-full border-b-2 border-primary">
     <div
-      class="m-auto flex h-16 max-w-screen-2xl items-center justify-between gap-2 px-2 py-5 sm:px-10 lg:h-20 2xl:px-0"
+      class="m-auto flex h-16 max-w-screen-2xl items-center justify-between gap-2 px-5 sm:px-10 md:h-20 2xl:px-0"
     >
+      <IconBurger class="md:!hidden" @click="openNavigation" />
+
       <TheHeaderLogo />
 
-      <div class="w-2/3 lg:w-2/5">
-        <TheHeaderSearch />
-      </div>
-
       <TheHeaderNavigation />
+
+      <TheHeaderOverlays />
     </div>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { openNavigation } = useInteractivityStore()
+</script>
 
 <style scoped></style>
