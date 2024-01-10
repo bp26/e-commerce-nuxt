6451ec02e578b6db58c9f7c2eca605e96384a23c
bp26 <aleshak260006@gmail.com>
2024-01-10 14:27:38 +0300

    feat: add TheHeaderOverlasyCartPanel component

---
+++ components/TheHeaderOverlaysCartPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <OverlayPanel ref="panel"><div>cart</div></OverlayPanel>
+</template>
+
+<script setup lang="ts">
+import type OverlayPanel from 'primevue/overlaypanel'
+
+const panel = ref<OverlayPanel>()
+defineExpose({ panel })
+</script>
+
+<style scoped></style>
