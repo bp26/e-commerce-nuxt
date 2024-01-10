b355d6a171d327548917da5dcb4b9e8445cbb87c
bp26 <aleshak260006@gmail.com>
2024-01-10 14:17:43 +0300

    feat: update TheHeaderNavigation component

--- components/TheHeaderNavigation.vue
+++ components/TheHeaderNavigation.vue
@@ -1,34 +1,20 @@
 <template>
-  <TheHeaderNavigationUpper :links="links" />
-  <TheHeaderNavigationBottom :links="links" />
+  <nav class="hidden md:block">
+    <ul class="flex gap-2 xl:gap-6">
+      <li v-for="link in NAVIGATION_LINKS" :key="link.to" class="flex-1">
+        <BaseLinkText :to="link.to" :active="isActiveLink(link.to)">
+          {{ link.text }}
+        </BaseLinkText>
+      </li>
+    </ul>
+  </nav>
+
+  <TheHeaderNavigationDrawer />
 </template>
 
 <script setup lang="ts">
-const links = [
-  {
-    to: '/',
-    text: 'Home',
-    icon: 'pi-home'
-  },
-  {
-    to: '/about',
-    text: 'About',
-    icon: 'pi-info-circle'
-  },
-  {
-    to: '/contact',
-    text: 'Contact',
-    icon: 'pi-id-card'
-  },
-  {
-    to: '/profile',
-    text: 'Profile',
-    icon: 'pi-user'
-  },
-  {
-    to: '/cart',
-    text: 'Cart',
-    icon: 'pi-shopping-cart'
-  }
-]
+import { NAVIGATION_LINKS } from '~/constants'
+const isActiveLink = useActiveLink()
 </script>
+
+<style scoped></style>
