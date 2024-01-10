b468acf44898963ba2cd1ff4914ae531b5378872
bp26 <aleshak260006@gmail.com>
2024-01-10 14:29:18 +0300

    feat: add TheHeaderOverlaysSearchDrawer component

---
+++ components/TheHeaderOverlaysSearchDrawer.vue
@@ -0,0 +1,12 @@
+<template>
+  <BaseDrawerMobile v-model:visible="isSearchOpen">
+    <TheHeaderOverlaysSearchBar />
+  </BaseDrawerMobile>
+</template>
+
+<script setup lang="ts">
+const store = useInteractivityStore()
+const { isSearchOpen } = storeToRefs(store)
+</script>
+
+<style scoped></style>
