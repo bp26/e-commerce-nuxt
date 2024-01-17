d85cadbbd0381426c462ec3c9be433a3ce3e0fc0
bp26 <aleshak260006@gmail.com>
2024-01-17 17:42:12 +0300

    feat: add method getFilters to ProductService

--- server/service/productService.ts
+++ server/service/productService.ts
@@ -1,3 +1,5 @@
+import { buildMinmaxFilter } from '~/server/helpers/buildMinmaxFilter'
+import { buildInclusionFilter } from '~/server/helpers/buildInclusionFilter'
 import { buildProductPrismaQuery } from '~/server/helpers/buildProductPrismaQuery'
 import productRepository from '~/server/db/repositories/productRepository'
 import { ProductQuery } from '~/server/types'
@@ -9,6 +11,37 @@ class ProductService {
     const count = await productRepository.getProductsCount(productPrismaQuery.where)
 
     return { products, count }
+  }
+
+  async getFilters(queries: ProductQuery) {
+    const productPrismaQuery = buildProductPrismaQuery(queries)
+
+    const [categoryGrouping, totalCategoryGrouping] = await Promise.all([
+      productRepository.groupProductsByCount('category', productPrismaQuery.where),
+      productRepository.groupProductsByCount('category')
+    ])
+
+    const [brandGrouping, totalBrandGrouping] = await Promise.all([
+      productRepository.groupProductsByCount('brand', productPrismaQuery.where),
+      productRepository.groupProductsByCount('brand')
+    ])
+
+    const [priceAggregation, totalPriceAggregation] = await Promise.all([
+      productRepository.aggregateProductsByMinMax('price', productPrismaQuery.where),
+      productRepository.aggregateProductsByMinMax('price')
+    ])
+
+    const [ratingAggregation, totalRatingAggregation] = await Promise.all([
+      productRepository.aggregateProductsByMinMax('rating', productPrismaQuery.where),
+      productRepository.aggregateProductsByMinMax('rating')
+    ])
+
+    return {
+      category: buildInclusionFilter('category', categoryGrouping, totalCategoryGrouping),
+      brand: buildInclusionFilter('brand', brandGrouping, totalBrandGrouping),
+      price: buildMinmaxFilter('price', priceAggregation, totalPriceAggregation),
+      rating: buildMinmaxFilter('rating', ratingAggregation, totalRatingAggregation)
+    }
   }
 }
 
